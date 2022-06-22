326ecd0a10126c9ed888c4c82a18473dc26a2ba0
Calvin <calvinkioko91@gmail.com>
2022-06-22 13:58:59 +0300

    Added elements to JS file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//DOM elements as functions
+const submit = document.querySelector('#submit');
+const search = document.querySelector('#search');
+const random = document.querySelector('#random');
+const cocktailsElement = document.querySelector('#cocktails');
+const resultHeading = document.querySelector('#result-heading');
+const selectedCocktail = document.querySelector('#selected-cocktail');
+

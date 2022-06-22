d55b3dca43a4a26614e61dc9906ff19bf0712bf3
Calvin <calvinkioko91@gmail.com>
2022-06-22 14:00:54 +0300

    Added functions to JS file

--- src/index.js
+++ src/index.js
@@ -6,3 +6,26 @@ const cocktailsElement = document.querySelector('#cocktails');
 const resultHeading = document.querySelector('#result-heading');
 const selectedCocktail = document.querySelector('#selected-cocktail');
 
+// Search for the cocktail by fetching form the api
+const searchCocktail = (e) => {
+    // Stop default search settings
+    e.preventDefault();
+  
+    // Clear any previously searched cocktails
+    cocktailsElement.innerHTML = '';
+    selectedCocktail.innerHTML = '';
+  
+    // Get the search which input by the user
+    const searchInput = search.value;
+    //console.log(searchInput);
+  
+    // Fetch search data from thecocktaildb API
+    if (searchInput.trim()) {
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          //console.log(data);
+          // Update the results heading with user search input
+          resultHeading.innerHTML = `<h2>Search results for '${searchInput}':</h2>`;
